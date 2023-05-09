0721b7607a4959e6a8a1ce89618688a26ea344a6
Shahid Afridi <shafridi9807@gmail.com>
2023-05-09 19:21:19 +0530

    fixing cors

--- app.js
+++ app.js
@@ -35,8 +35,12 @@ app.use(
 );
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
 app.use(xss());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URI,
+  })
+);
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
